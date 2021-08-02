902b13aa0644f2b2fb0d4874e2fdf06961e8cb02
runLeonov <leonovcasha@gmail.com>
2021-08-02 18:30:27 +0300

    Initial commit

---
+++ src/main/java/com/morethanheroic/uppercase/JsonFunctionApplication.java
@@ -0,0 +1,12 @@
+package com.morethanheroic.uppercase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonFunctionApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(JsonFunctionApplication.class, args);
+    }
+}
